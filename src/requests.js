5427d511a61939c49087a57a569525bdd631b942
Siddhant <84730469+Sidd-007@users.noreply.github.com>
2022-01-03 11:58:08 +0530

    Delete src directory

--- src/requests.js
+++
@@ -1,8 +0,0 @@
-const requests = {
-    fetchAiring : `/top/anime/1/airing`,
-    fetchPopular: `/top/anime/1/bypopularity`,
-    fetchUpcoming: `/top/anime/1/upcoming`,
-    fetchManga: `/top/manga`,
-}
-
-export default requests;
